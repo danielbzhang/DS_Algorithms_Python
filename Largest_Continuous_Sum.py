0cc3dfa65b260f6282ed5154ca8d10ee756b5ec6
Daniel B. Zhang <42197094+danielbzhang@users.noreply.github.com>
2022-03-07 20:49:51 -0500

    Update Largest_Continuous_Sum.py

--- Largest_Continuous_Sum.py
+++ Largest_Continuous_Sum.py
@@ -1,32 +1,5 @@
 # Problem
 # Given an array of integers (positive and negative) find the largest continuous sum. 
-
-def large_cont_sum(arr): 
-    
-    if len(arr) == 0:
-        return None
-    
-    max_sum = 0
-    
-    for i in range(len(arr)):
-        if arr[i]>0:
-            a = i
-            break
-            
-    for i in range(1, len(arr)):
-        if arr[-i] > 0:
-            b = i
-            break
-            
-    c = len(arr)-b+1
-    
-    print(f'Start: {arr[a]}')
-    print(f'End: {arr[c-1]}')
-    
-    for i in arr[a:c]:
-        max_sum += i
-        
-    return max_sum
   
   # TEST:
   large_cont_sum([1,2,-1,3,4,10,10,-10,-1])
